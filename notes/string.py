30fad32e9a8bc870e96309f321e1ad731393d079
ryanaguilera-commits <ryanaguilera@ucas-edu.net>
2025-09-08 14:41:58 -0600

    string notes

---
+++ notes/string.py
@@ -0,0 +1,15 @@
+#RA 7th string notes
+
+#examples
+first_name = input("what is your first name: \n").strip().capitalize()
+
+last_name = input("what is your last name").strip().capitalize()
+
+sentence = "the quick brown fox jumps over the lazy dog"
+
+print("hello", first_name + " " + last_name)
+ 
+
+
+
+
