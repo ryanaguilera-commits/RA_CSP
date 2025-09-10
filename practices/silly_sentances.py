13a137cbb0018fb945ce325a1485d16b9d427250
ryanaguilera-commits <ryanaguilera@ucas-edu.net>
2025-09-10 14:37:08 -0600

    finished silly sentances

---
+++ practices/silly_sentances.py
@@ -0,0 +1,8 @@
+#RA 7th Silly Sentances
+
+color1 = input("please input a color")
+pet = input("please enter an animal")
+adj1 = input("Please enter a adjective")
+pet2 = input("please enter a different animal")
+
+print("the",color1, pet, "fought with the", adj1, pet2)
