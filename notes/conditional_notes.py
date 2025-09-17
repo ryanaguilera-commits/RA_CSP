28d6664fd90bf74fbca6689d53ea7e7758d0d2bf
ryanaguilera-commits <ryanaguilera@ucas-edu.net>
2025-09-17 14:42:04 -0600

    finishing notes tommorow

---
+++ notes/conditional_notes.py
@@ -0,0 +1,16 @@
+#RA 7th Conitional notes
+##homework_done = input("is your homwork done\n").strip().capitalize()
+
+##if homework_done == "yes":
+    #print("yes you can go")
+##else:
+    #print("go do your homework")
+
+grade = 89
+
+if grade>89:
+    print(f"you have {grade}% thats an A")
+elif grade>79:
+    print(f"you have an {grade} that is a B")
+else:
+    print(f"you have an {grade} that's an F")
