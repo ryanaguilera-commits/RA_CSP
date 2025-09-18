fd2dccddb295878b2e60d9e8dbd436eb6ade929e
ryanaguilera-commits <ryanaguilera@ucas-edu.net>
2025-09-18 14:34:37 -0600

    Finsihed old enough

--- practices/old_enough.py
+++ practices/old_enough.py
@@ -1,13 +1,15 @@
 age = int(input("What is your age?"))
 
-if age > 18:
+if age >= 18:
     print("you are old enough to vote")
-elif age >16:
+elif age >= 16:
     print("You are old enough to drive")
-elif age >15:
+elif age >= 15:
     print("you are old enough to get your learners permit")
+elif age >= 4:
+    print("You can go to school")
 else:
-    print("go to school")
+    print("go get some sleep")
 
 
 
