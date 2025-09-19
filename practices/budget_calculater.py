ab64d1f8cd2d4f2afe956336c3b34b56faf609ee
ryanaguilera-commits <ryan.aguilera@ucas-edu.net>
2025-09-19 20:05:26 +0000

    raw

--- practices/budget_calculater.py
+++ practices/budget_calculater.py
@@ -1,8 +1,8 @@
 #RA 7th Budget Calculator
 
 income = input("how much is your income")
-rent = input("how much is uyour rent')
-utilities  = INPUT("how  much do you pay in utilities")
+rent = input("how much is your rent')
+utilities  = Input("how  much do you pay in utilities")
 transportation = Input("how much do you oay for transportation")
 groceries = input("how much do you pay for groceries")
 
