136b1fd71f4622a39f28ef6aa2634cbd228c9bbf
ryanaguilera-commits <ryanaguilera@ucas-edu.net>
2025-10-02 14:42:13 -0600

    C

---
+++ c_programming/notes/varibles.c
@@ -0,0 +1,19 @@
+//RA 7th Varibles
+#include <stdio.h>
+int main(void){
+    int grade = 95; //4 bytes
+    float pi = 3.14; //4 bytes
+    double long_pi = 3.14159265368; //8 bytes
+    char letter = 'A'; //1 byte
+    char name[] = "Ryan";
+    printf("%s did it!", name);
+    printf("You have a %d in the class that is a %c ", grade, letter);
+    return 0;
+}
+
+
+
+
+
+
+
