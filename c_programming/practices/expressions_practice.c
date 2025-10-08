c3eae354f3bc0b19b55ba3ec9f31ab88f00dea28
ryanaguilera-commits <ryanaguilera@ucas-edu.net>
2025-10-08 14:34:05 -0600

    expressions practice

---
+++ c_programming/practices/expressions_practice.c
@@ -0,0 +1,17 @@
+//RA 7th Expressions Practice
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    printf(" %d\n", 7 - 24/8*4 + 6);
+    printf(" %d\n", 18/3 - 7 + 2*5);
+    printf(" %d\n", 6*4/12 + 72/8 - 9);
+    printf(" %d\n", (17 - 6/2) + 4*3);
+    printf(" %d\n", -2*(1*4 - 2/2) + (6 + 2 - 3));
+    printf(" %d\n", -1*((3 - 4*7)/5) - 2*24/6);
+    printf(" %.0f\n", (3*pow(5,2)/15) - (5 - pow(2,2)));
+    printf(" %.0f\n", (pow(1,4)*pow(2,2)+pow(3,3)) - pow(2,5)/4);
+    printf(" %.0f\n", pow((22/2 - 2*5),2) + pow((4 - 6/6),2));
+    return 0;
+}
