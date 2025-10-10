ea2852859e0db1e924d45c5129f97d93dc97fa71
ryanaguilera-commits <ryan.aguilera@ucas-edu.net>
2025-10-10 06:05:25 +0000

    sorr

--- c_programming/practices/name_decorator.c
+++ c_programming/practices/name_decorator.c
@@ -4,11 +4,11 @@
 
 int main(void){
     char name[25];
-    char decor_one = "Xx"
-    char decor_two = "xX"
-    printf("please enter desired username: \n")
-    scanf(%s, name)
-    printf("%s%s%s",decor_one,name,decor_two)
+    char decor_one = "Xx";
+    char decor_two = "xX";
+    printf("please enter desired username: \n");
+    scanf(%s, name);
+    printf("%s%s%s",decor_one,name,decor_two);
 
 
 
---
+++ c_programming/practices/silly_sentances.c
@@ -0,0 +1,21 @@
+//RA 7th Silly Sentances
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char noun[25];
+    printf("please give me an noun:");
+    scanf("%s", noun);
+    printf("please give me a verb:");
+
+
+
+
+}
+
+
+
+
+
+
+
