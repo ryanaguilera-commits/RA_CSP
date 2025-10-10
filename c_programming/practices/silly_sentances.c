1d579ed49e00bcd1ce32d4960ddee08b680ba40a
ryanaguilera-commits <ryan.aguilera@ucas-edu.net>
2025-10-10 06:10:27 +0000

    yay

--- c_programming/practices/silly_sentances.c
+++ c_programming/practices/silly_sentances.c
@@ -3,11 +3,17 @@
 #include <string.h>
 
 int main(void){
-    char noun[25];
-    printf("please give me an noun:");
-    scanf("%s", noun);
-    printf("please give me a verb:");
+    char adjective[25];
+    char animal[25];
+    char verb[25];
 
+    printf("please give me an adjective:");
+    scanf("%s", adjective);
+    printf("please give me an active verb:");
+    scanf("%s", verb);
+    printf("please give me an animal:");
+    scanf("%s", animal);
+    printf("The %s %s was %s down the hill, through the barn and into the house.",adjective, animal, verb);
 
 
 
