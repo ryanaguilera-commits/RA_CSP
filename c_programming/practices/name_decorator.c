e7942d18e07c29b2c8f284a4d5c1d94040219258
ryanaguilera-commits <ryan.aguilera@ucas-edu.net>
2025-10-10 05:59:56 +0000

    sorr

---
+++ c_programming/practices/name_decorator.c
@@ -0,0 +1,22 @@
+//RA 7th Name Decorator
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char name[25];
+    char decor_one = "Xx"
+    char decor_two = "xX"
+    printf("please enter desired username: \n")
+    scanf(%s, name)
+    printf("%s%s%s",decor_one,name,decor_two)
+
+
+
+
+}
+
+
+
+
+
+
